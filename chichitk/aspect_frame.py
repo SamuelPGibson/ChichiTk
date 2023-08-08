30ddebf94d8404d0320bb3fe9dac29b5cabb9a95
Samuel Gibson <samuelpgibson12@gmail.com>
2023-08-08 16:47:07 -0600

    Improved aspect_frame
    * added get_dims() method
    * fixed set_aspect_ratio() method

--- chichitk/aspect_frame.py
+++ chichitk/aspect_frame.py
@@ -33,6 +33,9 @@ class AspectFrame(Frame):
         self.__config_callback = config_callback
         self.__size_callback = size_callback
 
+        self.__max_width, self.__max_height = 1, 1 # based on entire frame size
+        self.__width, self.__height = 1, 1 # based on aspect ratio within frame
+
         self.frame = Frame(self, bg=frame_bg)
 
         self.bind('<Configure>', self.__configure)
@@ -40,28 +43,41 @@ class AspectFrame(Frame):
 
     def __configure(self, event):
         '''places self.frame within self with the appropiate aspect ratio'''
-        # start by using the width as the controlling dimension
-        desired_width = event.width
-        desired_height = int(event.width / self.__aspect_ratio)
-
-        # if window is too tall to fit, use the height as controlling dimension
-        if desired_height > event.height:
-            desired_height = event.height
-            desired_width = int(event.height * self.__aspect_ratio)
-
+        self.__max_width, self.__max_height = event.width, event.height
+        self.__width, self.__height = self.__compute_dims()
+        
         self.frame.place(in_=self, relx=0.5, rely=0.5, anchor='center',
-                         width=desired_width, height=desired_height)
+                         width=self.__width, height=self.__height)
 
         if self.__config_callback:
             self.__config_callback(event)
         if self.__size_callback:
-            self.__size_callback(desired_width, desired_height)
+            self.__size_callback(*self.get_dims())
+
+    def __compute_dims(self):
+        '''returns width, height based on max_width, max_height, and aspect_ratio'''
+        # start by using the width as the controlling dimension
+        width = self.__max_width
+        height = int(self.__max_width / self.__aspect_ratio)
+
+        # if window is too tall to fit, use the height as controlling dimension
+        if height > self.__max_height:
+            height = self.__max_height
+            width = int(self.__max_height * self.__aspect_ratio)
+
+        return width, height
 
     def set_aspect_ratio(self, aspect_ratio:float):
         ''':param aspect_ratio: float - ratio of width / height'''
         self.__aspect_ratio = aspect_ratio
-        self.event_generate('<Configure>', when='now')
+        self.__width, self.__height = self.__compute_dims()
+        self.frame.place(in_=self, relx=0.5, rely=0.5, anchor='center',
+                         width=self.__width, height=self.__height)
 
     def get_aspect_ratio(self) -> float:
         '''returns aspect ratio: ratio of width / height'''
         return self.__aspect_ratio
+    
+    def get_dims(self):
+        '''returns inner frame width, height as tuple'''
+        return self.__width, self.__height
